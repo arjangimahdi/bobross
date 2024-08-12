1e7b69b0bb54c541af01a7e3c139a1ac7a3528ed
Mahdi Arjangi <github.mahdiarjangi.official@gmail.com>
2024-08-12 21:59:52 +0330

    initial commit

---
+++ src/zoom.ts
@@ -0,0 +1,16 @@
+class Zoom {
+  scale: number = 1
+  private isDragging : boolean = false
+
+  constructor() {
+
+  }
+
+  zoomInOut(scale: number, isIn: boolean) {}
+
+  startDragging() {}
+
+  dragging() {}
+
+  endDragging() {}
+}
